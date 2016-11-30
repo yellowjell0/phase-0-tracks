0cf778d876adf18edbe5cd1a260ae4c98b340254
Raymond Wesley Callahan <WesCallahan@Raymonds-MBP.attlocal.net>
2016-11-29 22:09:23 -0600

    puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Species:
+# Flufficus Petitamus
+
+# Characteristics: 
+# Color: Light Brown with White streaks
+# Hair Length: medium
+# Hair Type: Curly
+# Size: 7 lbs
+# Eye Color: Brown
+
+# Behavior:
+# Med Energy
+# Jumpy
+#Timid
