1e4d9532d17e8e32b2fc099295fd5358f00b1544
Raymond <Callahan>
2017-01-12 17:50:43 -0600

    Used data structure to create client form

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,66 +1,49 @@
-# Ask user to enter info and use info as appropriate data type
+# method: client entry program
+# input: user input, strings
+# steps: UI will ask user for entry, and entries will be stored inside of a data structure and reprinted in a easily readable fashion
+# output: arrays and hashes
+client_hash = Hash.new
+
+puts "Client entry program."
+
 puts "What is the clients name?"
 name = gets.chomp
+client_hash.store(:name, name)
+
+puts "What is the clients age?"
+age = gets.to_i
+client_hash.store(:age, age)
 
 puts "How many children does the client have?"
 children = gets.to_i
+client_hash.store(:children, children)
 
-puts "What is the age of the client?"
-age = gets.to_i
+puts "What theme of decor would the client like to have?"
+decor = gets.chomp
+client_hash.store(:decor, decor)
 
-puts "What is the decor theme the client desires?"
-decor_theme = gets.chomp
+puts "Was the client reffered by a previous client?"
+reffered = gets.chomp
+client_hash.store(:reffered, reffered)
 
-puts "Does the client have hardwood floors?"
-hardwood_floors = gets.chomp
+puts "Will the client be present upon designing?"
+present = gets.chomp
+client_hash.store(:present, present)
 
-puts "How many rooms in the house does the client have?"
-rooms = gets.to_i
-# Create a hash from the info entered and print the hash
+puts "Does the client have pets?"
+pets = gets.chomp
+client_hash.store(:pets, pets)
 
-client_information = {
-  name: name,
-  children: children,
-  age: age,
-  decor_theme: decor_theme,
-  hardwood_floors: hardwood_floors,
-  rooms: rooms
-}
-p client_information
+p client_hash
 
-# Give the opportunity to update the key unless done is entered. If the specific error key is entered, allow the option to change the data
-puts "Were there any errors (name, children, age, decor, floors, rooms)?"
-errors = ""
-no_errors = "done"
-until errors == no_errors
-errors = gets.chomp
-if errors == "name"
-  puts "What was the correction?"
-  client_information[:name] = gets.chomp
-  puts "Were there any other errors? Enter done when finished."
-  elsif errors == "children"
-  puts "What was the correction?"
-    client_information[:children] = gets.chomp
-    puts "Were there any other errors? Enter done when finished."
-    elsif errors == "age"
-    puts "What was the correction?"
-      client_information[:age] = gets.chomp
-      puts "Were there any other errors? Enter done when finished."
-      elsif errors == "decor"
-      puts "What was the correction?"
-        client_information[:decor] = gets.chomp
-        puts "Were there any other errors? Enter done when finished."
-        elsif errors == "floors"
-        puts "What was the correction?"
-          client_information[:hardwood_floors] = gets.chomp
-        puts "Were there any other errors? Enter done when finished."
-          elsif errors == "rooms"
-          puts "What was the correction?"
-            client_information[:rooms] = gets.chomp
-            puts "Were there any other errors? Enter done when finished."
+puts "Did were any errors? Type 'none' if no errors were made."
+error = gets.chomp
+if error == "none"
+  p client_hash
+  exit!
 end
-end
-# Print the latest version
-p client_information
-# Exit the program
-exit!
+puts "What is the correction?"
+correction = gets.chomp
+client_hash.store(error.to_sym, correction)
+
+p client_hash
