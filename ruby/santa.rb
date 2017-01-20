5047eba760c97e516aa1f18d2f2abc1ab5a3dfc4
Raymond <Callahan>
2017-01-19 19:04:05 -0600

    Created a santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,54 +1,46 @@
 class Santa
-    attr_accessor :gender
-    attr_reader :age, :ethnicity
-		def initialize(gender,ethnicity)
-		puts "Initializing Santa Instance"
-	  @gender = gender
+  attr_reader :ethnicity
+  attr_accessor :gender, :age
+
+	def initialize(gender, ethnicity)
 	  @ethnicity = ethnicity
+	  @gender = gender
 	  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	  @age = 0
+	   @age = 0
+		p "Initializing Santa instance ..."
 	end
-	  def speak
-		puts "Ho, Ho, Ho. Happy Holidays, #{@ethnicity}, #{@gender}."
-	end
-	  def eat_milk_and_cookies(cookie_type)
-		puts 'that was a good #{cookie_type}.'
-	end
-	  def celebrate_birthday
-	   # puts "Santa is now #{@age + 1}"
-	  end
-	  def get_mad_at(name)
-	    @reindeer_ranking.delete(name)
-	    @reindeer_ranking << name
-	    p @reindeer_ranking
-	  end
-	end
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
 
-genders = ["unsure", "ever-changing", "trans", "reverse-trans","agender","female","Bi-Gender","male","female","gender fluid","N/A", "He-She", "She-He"]
-ethnicities = ["rainbow", "dark-elf", "sea-foam", "Korean", "Black", "Latino","white","Japanese", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Gargoyle", "Wizard"]
+	def eat_milk_and_cookies(cookie)
+		p "That was a good #{cookie} cookie!" 
+	end
 
-genders.length.times do |i|
-  santas << Santa.new(genders[i], ethnicities[i])
-  puts "There are now #{santas.length} Santas."
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+	
+	def celebrate_birthday
+	  @age += 1
+	end
+	
+	def get_mad_at(reindeer)
+	  @reindeer_ranking.delete(reindeer)
+	  @reindeer_ranking << reindeer
+	end
+
 end
 
-Santa.new("Male", "Black").celebrate_birthday
-Santa.new("Bi-Gender", "Asian").get_mad_at("Rudolph")
-culauze = Santa.new("Male", "Green")
-culauze.gender = "Everything"
-puts "Culauze is #{culauze.ethnicity}, and her age is #{culauze.age}."
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "tri-gender", "hermaphodite", "flop-flop", "unsure", "sans-gender", "half-gender"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Marine Life", "Crab People", "Gnome", "Hot Cheeto(tm)", "Chamomile"]
 
-10000.times do |i|
-  Santa.new(genders.shuffle[i],ethnicities.shuffle[i])
-  puts "This Santa is a proud, #{genders.shuffle[i[rand(10000)]]}, #{ethnicities.shuffle[i[rand(10000)]]}, is proudly #{rand(140)} and ready to spread some joy!."
+1000.times do
+  santa = Santa.new(genders.sample, ethnicities.sample)
+   santa.age = rand(0..100)
+  puts "This Santa is a representative of the #{santa.ethnicity} race, is #{santa.age} years young, and uses the #{santa.gender} bathroom."
 end
   
-  
+
+# santa.celebrate_birthday
+# santa.get_mad_at("Dasher")
+# santa.age = 1
+# santa.ethnicity
+# santa.gender = "neutral"
