f62a5bc8e229f122b9794e04b7fe7d8b90d9bf37
Raymond <Callahan>
2017-01-29 23:25:58 -0600

    Created three different javascript functions and tested them

--- js/algos.js
+++ js/algos.js
@@ -1,25 +1,49 @@
 // method: longestWord
 // input: array
-// steps: set vavue for max, set return, iterate through each index of the array, 
+// steps: declare maxVal as 0,
+// declare result as 0,
+// iterate through the array using a conditional. 
+// If the length of the string is larger than the max value, the string length becomes
+// the max value, and the result becomes the string.
 // output: string
+
+
+// method: keyValueCheck
+// input: 2 hashes
+// steps: declare keys to equal an array of the keys of hash1,
+// loop through the keys array the amount of the length of the keys array starting at the 0 index,
+// using a conditional, if the key of the hash1 is equal to the key of hash2,
+// return true, otherwise, return false
+// output: boolean
+
+// method: generateTestData
+// input: integer
+// steps: the randomInt function returns a random number with no decimal.
+// alphabet is declared as a string 
+// array is declared as an empty array
+// loop through the integer argument as long as it is greater than 0
+// string is declared as an empty string
+// randomLength is declared as the randomInt function with a max of 10.
+// As long as the randomLength is not equal to zero, declare a random letter
+// using the randomInt function on the apphabet with the length of the alphabet as 
+// the argument minus one, then set the string value to as many random letters as the 
+// loop allows
+// Push the string into the array, and return it
+// output: string
+
 function longestWord(array) {
   var maxVal = 0;
   result = "";
-  array.forEach(function(s) {
-  if (s.length > maxVal) {
-    result = s;
-    maxVal = s.length;
+  array.forEach(function(string) {
+  if (string.length > maxVal) {
+    maxVal = string.length;
+    result = string;
   }
   });
   console.log(result);
 }
 
 
-
-// method: keyValueCheck
-// input: 2 hashes
-// steps: iterate through a hash to see if there are any equivalents in the pair of hashes and return a boolean based on the results
-// output: boolean
 function keyValueCheck(hash1,hash2) {
   var keys = Object.keys(hash1);
   for (var i = 0; i < keys.length; i++) {
@@ -35,10 +59,6 @@ var hash2 = {name: "Tamir", age: 54};
 keyValueCheck(hash1,hash2);
 
 
-// method: generateTestData
-// input: integer
-// steps: take an integer as an argument and iterate through an array of strings according to the integer. Then return a random string that is the length of the argument, but random letters of the string
-// output: string
 function generateTestData(n) {
   function randomInt(max) {
     return Math.floor(Math.random() * max) + 1;
@@ -60,8 +80,8 @@ function generateTestData(n) {
   //Generates an array, prints the array(*10), feeds the array to longestWord, and prints the result.
 for (var i = 0; i < 10; i++) {
   var testData = generateTestData(4);
-  console.log(longestWord(testData));
-}
+  console.log(testData);
+ longestWord(testData);
   
   // Test longestWord
 var words = ["long phrase","longest phrase","longer phrase"];
