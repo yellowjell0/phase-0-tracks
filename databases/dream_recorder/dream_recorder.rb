4887a5d5802d3906fcfa581186c65aa657547161
Raymond <Callahan>
2017-02-06 05:31:55 -0600

    Used sqlite3 and ruby to make an app that stores data

--- databases/dream_recorder/dream_recorder.rb
+++ databases/dream_recorder/dream_recorder.rb
@@ -1,13 +1,13 @@
 # The dream recorder is designed to store and keep track of the behaviour 
-# of the users dreams in a weekly and monthly fashion, giving the user the
-# ability to recall dreams, and to check how regularly some of the same types are occuring.
+# of the users dreams in a weekly fashion, giving the user a .
 
 require 'sqlite3'
 require 'faker'
 
+
+
 db = SQLite3::Database.new("dreams.db")
 db.results_as_hash = true
-counter = 1
 
 create_dream_table = <<-SQL
 CREATE TABLE IF NOT EXISTS dreams(
@@ -22,23 +22,24 @@ SQL
 
 db.execute(create_dream_table)
 
-def daily_dream_log(db, description, vivid, recurring, tone)
+def daily_dream_log_method(db, description, vivid, recurring, tone)
 	db.execute("INSERT INTO dreams (description, vivid, recurring, tone) VALUES (?, ?, ?, ?)", [description,
-		vivid, recurring, tone])
+	vivid, recurring, tone])
 end
 
 
 # UI
 
+
 puts "Hello. Welcome to the dream recorder, where the mysterious realm of fantasy 
-is explored and tracked through daily logs."
+is explored and tracked through your daily logs."
 puts "Can you describe the dream you had last night? Type 'n/a' if you 
 forgot or don't know what or if you dreamt."
 
 description = gets.chomp
 
 if description == "n/a"
-	daily_dream_log(db, 'no dream', 'false', 'false', 'no dream')
+	daily_dream_log_method(db, 'no dream', 'false', 'false', 'no dream')
 	exit!
 else
 end
@@ -55,41 +56,41 @@ puts "Would you describe this dream as a nightmare, blissful, or regular?"
 
 tone = gets.chomp
 
+daily_dream_log_method(db, description, vivid, recurring, tone)
+
 dreams_log_array = db.execute('SELECT * FROM dreams')
 
+weekly_array = dreams_log_array 
 
-weekly_array = dreams_log_array.combination(7).to_a
-p weekly_array
+no_dreams = 0
+vivid_dreams = 0
+recurring_dreams = 0
+nightmares = 0
+regular_dreams = 0
+blissful_dreams = 0
 
+if weekly_array.length % 7 == 0 
+	
+weekly_array = weekly_array[-7..-1]
 
-
-# def weekly_report
-	no_dreams = 0
-	vivid_dreams = 0
-	recurring_dreams = 0
-	nightmares = 0
-	regular_dreams = 0
-	blissful_dreams = 0
-
+puts "For the week, your dreams have been:"
 	weekly_array.each do |dreams_log_array|
-		puts "For the week, your dreams have been:"
-		dreams_log_array.each_with_index do |hash, index|
-		puts "On #{dreams_log_array[index]['date']}, you dreamt #{dreams_log_array[index]['description']}."
-		no_dreams += 1 if dreams_log_array[index].has_value?("no dream")
-		vivid_dreams += 1 if dreams_log_array[index]['vivid'] == "true"
-		recurring_dreams += 1 if dreams_log_array[index]['recurring'] == "true"
-		nightmares += 1 if dreams_log_array[index]['tone'] == "nightmare"
-		regular_dreams += 1 if dreams_log_array[index]['tone'] == "regular"
-		blissful_dreams += 1 if dreams_log_array[index]['tone'] == "blissful"
-		end
-
+				puts "On #{dreams_log_array['date']}, you dreamt #{dreams_log_array['description']}."
+				no_dreams += 1 if dreams_log_array.has_value?("no dream")
+				vivid_dreams += 1 if dreams_log_array['vivid'] == "true"
+				recurring_dreams += 1 if dreams_log_array['recurring'] == "true"
+				nightmares += 1 if dreams_log_array['tone'] == "nightmare"
+				regular_dreams += 1 if dreams_log_array['tone'] == "regular"
+				blissful_dreams += 1 if dreams_log_array['tone'] == "blissful"
 	end
-p no_dreams
-p vivid_dreams
-# end
+	puts "Now lets tally up your dream types for the week."
+	puts "Wait, what? Nights without dreams: #{no_dreams}." 
+	puts "Pinch yourself! Nights with vivid dreams: #{vivid_dreams}."
+	puts "Deja Vu! Nights with recurring dreams: #{recurring_dreams}."
+	puts "Uh-oh, Nights with nightmares: #{nightmares}."
+	puts "Boring! Nights with regular dreams #{regular_dreams}."
+	puts "Can't this just be the real world? Nights with blissful dreams: #{blissful_dreams}."
 
-
-
-
-
+end
+	
 
